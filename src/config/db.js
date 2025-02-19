71398f466d30c9b89fc45e34ce8496d08ec6ff61
Adriansenai9 <128429963+Adriansenai9@users.noreply.github.com>
2025-02-18 22:29:05 -0300

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb+srv://adrian:A12miasE41ntLYQ0@cluster0.k0eev.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+        await mongoose.connect("");
         console.log("MongoDB connected");
     } catch (err) {
         console.error(`Error: ${err}`);
